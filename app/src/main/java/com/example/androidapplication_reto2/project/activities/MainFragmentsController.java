f74ed26bfddb56a66c4e0685b623d961447b9b16
jarsss8 <adriancgs@gmail.com>
2020-01-14 10:32:07 +0100

    Creada la ventana de mostrar datos del usuario, colocado el boton en el appbar menu y funciona cuando lo tocas

--- app/src/main/java/com/example/androidapplication_reto2/project/activities/MainFragmentsController.java
+++ app/src/main/java/com/example/androidapplication_reto2/project/activities/MainFragmentsController.java
@@ -1,12 +1,14 @@
 package com.example.androidapplication_reto2.project.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import android.os.Bundle;
+import android.view.Menu;
 
 
 import com.example.androidapplication_reto2.R;
@@ -21,6 +23,9 @@ public class MainFragmentsController extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         DrawerLayout drawerLayout=findViewById(R.id.drawer);
         NavigationView navigationView=findViewById(R.id.nav_view);
--- app/src/main/java/com/example/androidapplication_reto2/project/activities/navigationfragments/SeeAndModifyUserDataFragment.java
+++ app/src/main/java/com/example/androidapplication_reto2/project/activities/navigationfragments/SeeAndModifyUserDataFragment.java
@@ -3,31 +3,61 @@ package com.example.androidapplication_reto2.project.activities.navigationfragme
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.example.androidapplication_reto2.R;
+import com.example.androidapplication_reto2.project.activities.MainFragmentsController;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SeeAndModifyUserDataFragment extends Fragment {
 
+    private TextView lbLogin, lbFullName, lbPrivilege, lbEmail;
 
-    public SeeAndModifyUserDataFragment() {
-        // Required empty public constructor
-    }
-
-    //ADD CREDIT CARD POP UP
+    //Todo ADD CREDIT CARD POP UP
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View root=inflater.inflate(R.layout.fragment_see_and_modify_user_data, container, false);
+        View root = inflater.inflate(R.layout.fragment_see_and_modify_user_data, container, false);
+
+        setHasOptionsMenu(true);
+
+        lbLogin = root.findViewById(R.id.lbUserNameUserData2);
+        lbFullName = root.findViewById(R.id.lbFullNameUserData2);
+        lbEmail = root.findViewById(R.id.lbEmailUserData2);
+        lbPrivilege = root.findViewById(R.id.lbPrivilegeUserData2);
+
+
+
         return root;
     }
 
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.user_data_menu,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.btEditUserData:
+                Toast.makeText(getContext(), "Hey", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
